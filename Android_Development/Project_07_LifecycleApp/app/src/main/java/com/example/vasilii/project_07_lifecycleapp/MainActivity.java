476e5a8858aae08c93e2d521808b7c124ef1759f
Vasilii Kovalev <vasya.kowalev@yandex.ru>
2018-11-23 23:37:49 +0300

    Add Android Development Lesson (week 06)

---
+++ Android_Development/Project_07_LifecycleApp/app/src/main/java/com/example/vasilii/project_07_lifecycleapp/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.vasilii.project_07_lifecycleapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "LifeCycle";
+
+    private TextView mInfoLog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInfoLog = findViewById(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_LONG).show();
+        Log.i(TAG, "onDestroy");
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.clickMeButton:
+                mInfoLog.setText(getString(R.string.alreadyLaunchedText));
+                break;
+            case R.id.exitAppButton:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+}
