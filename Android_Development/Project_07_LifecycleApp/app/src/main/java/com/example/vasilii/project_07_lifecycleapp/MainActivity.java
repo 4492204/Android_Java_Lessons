959c86829b7c3bf5920b2220f419cc46cc0bbf86
Vasilii Kovalev <vasya.kowalev@yandex.ru>
2018-12-01 19:21:44 +0300

    Fix for project 08

--- Android_Development/Project_07_LifecycleApp/app/src/main/java/com/example/vasilii/project_07_lifecycleapp/MainActivity.java
+++ Android_Development/Project_07_LifecycleApp/app/src/main/java/com/example/vasilii/project_07_lifecycleapp/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mInfoLog = findViewById(R.layout.activity_main);
+        mInfoLog = findViewById(R.id.textView);
         Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_LONG).show();
         Log.i(TAG, "onCreate");
     }
